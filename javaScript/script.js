050235e92acbc3388077ddb4466b6c9346108a52
Eben Keys <ebenezerkwekuagyepong@gmail.com>
2023-02-03 03:38:10 +0000

    new file:   .vscode/settings.json 	modified:   cssFolder/style.css 	new file:   cssFolder/swiper-bundle.min.css 	modified:   index.html 	modified:   javaScript/script.js 	new file:   javaScript/swiper-element-bundle.js

--- javaScript/script.js
+++ javaScript/script.js
@@ -92,3 +92,17 @@ moddalClose.forEach((moddalClose) =>{
         })
     })
 })
+
+/*================ PORTFOLIO SWIPER ================= */
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
